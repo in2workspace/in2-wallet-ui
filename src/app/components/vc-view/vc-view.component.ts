02904eb2d3dc91ac9dd372a73e49d5863687d4d6
IN2\albertorr <alberto.rubio@in2.es>
2023-06-20 12:49:15 +0200

    Fix code smells

--- src/app/components/vc-view/vc-view.component.ts
+++ src/app/components/vc-view/vc-view.component.ts
@@ -13,19 +13,14 @@ export interface VerifiableCredential{
 @Component({
   selector: 'app-vc-view',
   templateUrl: './vc-view.component.html',
-  styleUrls: ['./vc-view.component.scss'],
   standalone: true,
   imports: [IonicModule,QRCodeModule],
 })
 export class VcViewComponent implements OnInit{
-  @Output() availableDevices: EventEmitter<MediaDeviceInfo[]> =
-  new EventEmitter();
-  @Output() qrCode: EventEmitter<string> =
-  new EventEmitter();
+
   @Input() credentialInput: VerifiableCredential = {credentialSubject:{firstName:"",familyName:"",nom:"",cognoms:""},id:"",vcType:['','']} ;
   cred: VerifiableCredential ={credentialSubject:{firstName:"",familyName:"",nom:"",cognoms:""},id:"",vcType:['','']};
-  constructor() { 
-  }
+
 
   ngOnInit(): void {
     this.cred = this.credentialInput
