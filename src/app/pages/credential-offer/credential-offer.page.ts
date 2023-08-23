508f94a50c78fe5d05e493a1646a435b3bd96d68
IN2\albertorr <alberto.rubio@in2.es>
2023-08-23 12:11:30 +0200

    update view

--- src/app/pages/credential-offer/credential-offer.page.ts
+++ src/app/pages/credential-offer/credential-offer.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { WalletService } from 'src/app/services/wallet.service';
-
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { StorageService } from 'src/app/services/storage.service';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-credential-offer',
   templateUrl: './credential-offer.page.html',
   styleUrls: ['./credential-offer.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule,RouterModule]
 })
 export class CredentialOfferPage implements OnInit {
 
@@ -17,9 +19,14 @@ export class CredentialOfferPage implements OnInit {
     ) { }
   issuersList = [];
   didsList =[]
+  login = new FormGroup<any>({
+    username: new FormControl('', {nonNullable: true}),
+    password: new FormControl('', {nonNullable: true}),
+});
   ngOnInit() {
     this.walletService.getAllDIDs().subscribe(response => {console.log(response);this.didsList=response})
 
   }
-
+  onSubmit(){
+  }
 }
