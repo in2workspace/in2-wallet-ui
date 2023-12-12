2d86f53fe4388fd09ec82cd62120d76e5e0a6bc3
Oriol Canadés <oriol.canades@in2.es>
2023-12-12 22:34:13 +0100

    Update environment routes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
-  data_url: 'https://wallet-data:8080',
-  wca_url: 'https://wallet-creation-application:8080',
+  data_url: 'https://localhost:8086',
+  wca_url: 'https://localhost:8081',
   loginParams: {
     has_login: true,
-    login_url: 'https://wallet-identity-provider:8080/realms/wallet',
+    login_url: 'https://localhost:9099/realms/wallet',
     client_id: 'auth-client',
     scope: 'openid profile email offline_access',
     grant_type: 'code'
   },
   registerParams: {
     has_register: true,
-    register_url: 'https://wallet-user-registry:8080',
+    register_url: 'https://localhost:8085',
     client_id: '',
     client_secret: '',
     grant_type: ''
