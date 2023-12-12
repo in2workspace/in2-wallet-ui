61ade1c5f5d3a5689e409a7ff453b576299f931d
Oriol Canadés <83498869+oriolcanadesin2@users.noreply.github.com>
2023-12-12 23:09:04 +0100

    Hotfix/v2.0.0 2 (#18)
    * Update environment routes
    
    * Update environment routes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
-  data_url: 'https://localhost:8086',
-  wca_url: 'https://localhost:8081',
+  data_url: 'http://localhost:8086',
+  wca_url: 'http://localhost:8081',
   loginParams: {
     has_login: true,
-    login_url: 'https://localhost:9099/realms/wallet',
+    login_url: 'http://localhost:9099/realms/wallet',
     client_id: 'auth-client',
     scope: 'openid profile email offline_access',
     grant_type: 'code'
   },
   registerParams: {
     has_register: true,
-    register_url: 'https://localhost:8085',
+    register_url: 'http://localhost:8085',
     client_id: '',
     client_secret: '',
     grant_type: ''
