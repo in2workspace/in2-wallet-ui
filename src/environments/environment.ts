c7f096d527b285dc802ec5cf757b386fb0d36bd4
Oriol Canadés <83498869+oriolcanadesin2@users.noreply.github.com>
2023-12-12 22:37:48 +0100

    Feature/v2.0.0 snapshot (#15)
    * Fixes from SonarCloud issues
    
    * Fixes from SonarCloud issues
    
    * Fixes from SonarCloud issues
    
    * Fixes from SonarCloud issues
    
    * Update ports
    
    * Update environment routes
    
    * Update environment routes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  data_url: 'https://wallet-data:8080',
-  wca_url: 'https://wallet-creation-application:8080',
+  data_url: 'https://localhost:8086',
+  wca_url: 'https://localhost:8081',
   loginParams: {
     has_login: true,
-    login_url: 'https://wallet-identity-provider:8080/realms/wallet',
+    login_url: 'https://localhost:9099/realms/wallet',
     client_id: 'auth-client',
     scope: 'openid profile email offline_access',
     grant_type: 'code'
