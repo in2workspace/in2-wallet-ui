ef86b22ea86a6bdd78eedd4378e68b7fef69b35d
IN2\albertorr <alberto.rubio.in2@gmail.com>
2023-12-20 12:55:13 +0100

    add docker entornos variables dinamicas

--- src/environments/environment.customdev.ts
+++ src/environments/environment.customdev.ts
@@ -1,21 +1,26 @@
 export const environment = {
   production: false,
-  api_url: 'https://yourapiurl',
-  data_url: 'https://yourdataurl',
-  wca_url: 'https://yourwcaurl',
-  crypto_url: 'https://yourcyptourl',
-  loginParams:{
+  data_url: window["env"]["apiUrl"] || 'http://localhost:8086',
+  wca_url: window["env"]["dataUrl"] || 'http://localhost:8081',
+  loginParams: {
     has_login: true,
-    login_url: 'https://yourloginurl',
-    client_id: 'client',
-    client_secret:'secret',
-    grant_type:'password'
+    login_url: window["env"]["loginUrl"] || 'http://localhost:9099/realms/wallet',
+    client_id: 'auth-client',
+    scope: 'openid profile email offline_access',
+    grant_type: 'code'
   },
-  registerParams:{
+  registerParams: {
     has_register: true,
-    register_url: 'https://yourregisterurl',
-    client_id:'',
-    client_secret:'',
-    grant_type:''
+    register_url: window["env"]["registerUrl"] || 'http://localhost:8085',
+    client_id: '',
+    client_secret: '',
+    grant_type: ''
   },
+  walletUri: {
+    execute_content_uri: window["env"]["execContUri"] || '/api/v2/execute-content',
+    verifiable_presentation_uri: window["env"]["vPUri"] || '/api/v2/verifiable-presentation',
+    credentials_uri: window["env"]["credUri"] || '/api/v2/credentials',
+    credentials_by_id_uri: window["env"]["credIdUri"] || '/api/v2/credentials?credentialId=',
+    users_uri: window["env"]["userUri"] || '/api/v2/users',
+  }
 };
