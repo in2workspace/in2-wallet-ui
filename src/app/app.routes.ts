5a6f148dbf2ead81b110630cd687a279d94668c5
IN2\albertorr <alberto.rubio@in2.es>
2024-02-12 12:38:57 +0100

    delete unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,30 +7,10 @@ export const routes: Routes = [
     redirectTo: '/tabs/home',
     pathMatch: 'full',
   },
-
-  {
-    path: 'privacy-policy',
-    loadComponent: () =>
-      import('./pages/privacy-policy/privacy-policy.page').then(
-        (m) => m.PrivacyPolicyPage
-      ),
-  },
-  {
-    path: 'terms-of-use',
-    loadComponent: () =>
-      import('./pages/terms-of-use/terms-of-use.page').then(
-        (m) => m.TermsOfUsePage
-      ),
-  },
- 
-  {
-    path: 'faqs',
-    loadComponent: () =>
-      import('./pages/faqs/faqs.page').then((m) => m.FaqsPage),
-  },
   {
     path: 'tabs',
-    loadComponent: () => import('./pages/tabs/tabs.page').then(m => m.TabsPage),
+    loadComponent: () =>
+      import('./pages/tabs/tabs.page').then((m) => m.TabsPage),
     children: [
       {
         path: 'home',
@@ -46,51 +26,51 @@ export const routes: Routes = [
             (m) => m.CredentialsPage
           ),
       },
-       {
+      {
         path: 'settings',
         canActivate: [AutoLoginPartialRoutesGuard],
-    
+
         loadComponent: () =>
           import('./pages/settings/settings.page').then((m) => m.SettingsPage),
-          children:[
-           ]
+        children: [],
       },
       {
         path: 'language-selector',
         canActivate: [AutoLoginPartialRoutesGuard],
-    
+
         loadComponent: () =>
           import('./pages/language-selector/language-selector.page').then(
             (m) => m.LanguageSelectorPage
           ),
       },
 
-      
-  {
-    path: 'camera-selector',
-    canActivate: [AutoLoginPartialRoutesGuard],
-    loadComponent: () =>
-      import('./pages/camera-selector/camera-selector.page').then(
-        (m) => m.CameraSelectorPage
-      ),
-  },
-  
-  {
-    path: 'vc-selector',
-    canActivate: [AutoLoginPartialRoutesGuard],
-    loadComponent: () =>
-      import('./pages/vc-selector/vc-selector.page').then(
-        (m) => m.VcSelectorPage
-      ),
-  },
+      {
+        path: 'camera-selector',
+        canActivate: [AutoLoginPartialRoutesGuard],
+        loadComponent: () =>
+          import('./pages/camera-selector/camera-selector.page').then(
+            (m) => m.CameraSelectorPage
+          ),
+      },
+
+      {
+        path: 'vc-selector',
+        canActivate: [AutoLoginPartialRoutesGuard],
+        loadComponent: () =>
+          import('./pages/vc-selector/vc-selector.page').then(
+            (m) => m.VcSelectorPage
+          ),
+      },
       {
         path: '',
         redirectTo: '/tabs/home',
-        pathMatch: 'full'
-      }
-    ]
+        pathMatch: 'full',
+      },
+    ],
   },
-  { path: 'callback',
-  loadComponent: () => import('./pages/callback/callback.page').then( m => m.CallbackPage)
-  }
+  {
+    path: 'callback',
+    loadComponent: () =>
+      import('./pages/callback/callback.page').then((m) => m.CallbackPage),
+  },
 ];
