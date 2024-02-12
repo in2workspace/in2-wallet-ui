d126d1e957801e3d5925b265acaaf4b398f5de5f
IN2\albertorr <alberto.rubio@in2.es>
2024-02-12 09:59:11 +0100

    no message

--- karma.conf.js
+++ karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
+        { type: 'cobertura' },
         { type: 'text-summary' }
       ]
     },
