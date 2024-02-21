5cf83fba941301fd70f81e2f28b46f1880c55475
IN2\albertorr <alberto.rubio@in2.es>
2024-02-21 15:24:44 +0100

    updated environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,22 +1,22 @@
 export const environment = {
   production: true,
-  server_url:window["env"]["serverUrl"] || 'http://localhost:8081',
-  iam_url: window["env"]["iamUrl"] || 'http://localhost:9099',
-  websocket_url: window["env"]["websocketUrl"] || 'ws://localhost:8081',
+  server_url:window["env"]["server_url"] || 'http://localhost:8081',
+  iam_url: window["env"]["iam_url"] || 'http://localhost:9099',
+  websocket_url: window["env"]["websocket_url"] || 'ws://localhost:8081',
   iam_params: {
-    iam_uri: window["env"]["iamUri"] || '/realms/wallet',
+    iam_uri: window["env"]["iam_uri"] || '/realms/wallet',
     client_id: window["env"]["client_id"] ||  'auth-client',
     scope: window["env"]["scope"] || 'openid profile email offline_access',
     grant_type: window["env"]["grant_type"] || 'code'
   },
   server_uri: {
-    execute_content_uri: window["env"]["execContUri"] || '/api/v2/execute-content',
-    request_credential_uri: '/api/v2/request-credential',
-    verifiable_presentation_uri: window["env"]["vPUri"] || '/api/v2/verifiable-presentation',
-    credentials_uri: window["env"]["credUri"] || '/api/v2/credentials',
-    credentials_by_id_uri: window["env"]["credIdUri"] || '/api/v2/credentials?credentialId=',
-    users_uri: window["env"]["userUri"] || '/api/v2/users',
-    ebsi_did_uri: window["env"]["ebsiDid"] || '/api/v2/ebsi-did'
+    execute_content_uri: window["env"]["execute_content_uri"] || '/api/v2/execute-content',
+    request_credential_uri: window["env"]["request_credential_uri"] || '/api/v2/request-credential',
+    verifiable_presentation_uri: window["env"]["verifiable_presentation_uri"] || '/api/v2/verifiable-presentation',
+    credentials_uri: window["env"]["credentials_uri"] || '/api/v2/credentials',
+    credentials_by_id_uri: window["env"]["credentials_by_id_uri"] || '/api/v2/credentials?credentialId=',
+    users_uri: window["env"]["users_uri"] || '/api/v2/users',
+    ebsi_did_uri: window["env"]["ebsi_did_uri"] || '/api/v2/ebsi-did'
   },
-  websocket_uri: window["env"]["websocketUri"] || '/api/v2/pin'
+  websocket_uri: window["env"]["websocket_uri"] || '/api/v2/pin'
 };
