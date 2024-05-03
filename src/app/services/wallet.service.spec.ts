ab7edb0f59bd47746b7c419edb5cf9af45df55a9
Daniel Guillen <daniel.guillen@col.vueling.com>
2024-05-03 13:25:52 +0200

    test

--- src/app/services/wallet.service.spec.ts
+++ src/app/services/wallet.service.spec.ts
@@ -1,8 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { WalletService } from './wallet.service';
 import { environment } from 'src/environments/environment';
-import { CredentialStatus, VerifiableCredential } from '../interfaces/verifiable-credential';
+import {
+  CredentialStatus,
+  VerifiableCredential,
+} from '../interfaces/verifiable-credential';
 
 interface VCReply {
   selectedVcList: any[];
@@ -17,7 +23,7 @@ describe('WalletService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [WalletService]
+      providers: [WalletService],
     });
     service = TestBed.inject(WalletService);
     httpTestingController = TestBed.inject(HttpTestingController);
@@ -29,87 +35,100 @@ describe('WalletService', () => {
 
   it('should fetch VC in CBOR format', () => {
     const mockCredential: VerifiableCredential = {
-      "@context": ["https://www.w3.org/ns/credentials/v1"],
-      "id": "test-credential-id",
-      "type": ["VerifiableCredential", "LEARCredentialEmployee"],
-      "issuer": {
-        "id": "did:web:provider.dome.fiware.dev"
+      '@context': ['https://www.w3.org/ns/credentials/v1'],
+      id: 'test-credential-id',
+      type: ['VerifiableCredential', 'LEARCredentialEmployee'],
+      issuer: {
+        id: 'did:web:provider.dome.fiware.dev',
       },
-      "issuanceDate": "2024-04-02T09:23:22.637345122Z",
-      "validFrom": "2024-04-02T09:23:22.637345122Z",
-      "expirationDate": "2025-01-01T00:00:00Z",
-      "credentialSubject": {
-        "mandate": {
-          "id": "mandateId1",
-          "mandator": {
-            "organizationIdentifier": "orgId1",
-            "commonName": "Common Name",
-            "emailAddress": "email@example.com",
-            "serialNumber": "serialNumber1",
-            "organization": "Organization Name",
-            "country": "Country"
+      issuanceDate: '2024-04-02T09:23:22.637345122Z',
+      validFrom: '2024-04-02T09:23:22.637345122Z',
+      expirationDate: '2025-01-01T00:00:00Z',
+      credentialSubject: {
+        mandate: {
+          id: 'mandateId1',
+          mandator: {
+            organizationIdentifier: 'orgId1',
+            commonName: 'Common Name',
+            emailAddress: 'email@example.com',
+            serialNumber: 'serialNumber1',
+            organization: 'Organization Name',
+            country: 'Country',
           },
-          "mandatee": {
-            "id": "personId1",
-            "first_name": "John",
-            "last_name": "Doe",
-            "gender": "male",
-            "email": "test@example.com",
-            "mobile_phone": "+1234567890"
+          mandatee: {
+            id: 'personId1',
+            first_name: 'John',
+            last_name: 'Doe',
+            gender: 'male',
+            email: 'test@example.com',
+            mobile_phone: '+1234567890',
           },
-          "power": [
+          power: [
             {
-              "id": "powerId1",
-              "tmf_type": "Domain",
-              "tmf_domain": ["DOME"],
-              "tmf_function": "Onboarding",
-              "tmf_action": ["Execute"]
-            }
+              id: 'powerId1',
+              tmf_type: 'Domain',
+              tmf_domain: ['DOME'],
+              tmf_function: 'Onboarding',
+              tmf_action: ['Execute'],
+            },
           ],
-          "life_span": {
-            "start_date_time": "2024-04-02T09:23:22.637345122Z",
-            "end_date_time": "2025-01-01T00:00:00Z"
-          }
-        }
+          life_span: {
+            start_date_time: '2024-04-02T09:23:22.637345122Z',
+            end_date_time: '2025-01-01T00:00:00Z',
+          },
+        },
       },
-      status: CredentialStatus.ISSUED
+      status: CredentialStatus.ISSUED,
     };
     const mockResponse = 'mock-cbor-data';
 
-    service.getVCinCBOR(mockCredential).subscribe(response => {
+    service.getVCinCBOR(mockCredential).subscribe((response) => {
       expect(response).toEqual(mockResponse);
     });
 
-    const req = httpTestingController.expectOne(`${environment.server_url}/api/v1/vp/cbor`);
+    const req = httpTestingController.expectOne(
+      `${environment.server_url}/api/v1/vp/cbor`
+    );
     expect(req.request.method).toEqual('POST');
     req.flush(mockResponse);
   });
-
 
   it('should fetch VC in JWT format', () => {
     const mockCredentialId = 'test-credential-id';
     const mockResponse = 'mock-jwt-data';
 
-    service.getVCinJWT({ id: mockCredentialId } as VerifiableCredential).subscribe(response => {
-      expect(response).toEqual(mockResponse);
-    });
+    service
+      .getVCinJWT({ id: mockCredentialId } as VerifiableCredential)
+      .subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
 
-    const req = httpTestingController.expectOne(`${environment.server_url}/api/credentials/id?credentialId=${mockCredentialId}&format=vc_jwt`);
+    const req = httpTestingController.expectOne(
+      `${environment.server_url}/api/credentials/id?credentialId=${mockCredentialId}&format=vc_jwt`
+    );
     expect(req.request.method).toEqual('GET');
     req.flush(mockResponse);
   });
 
   it('should request a new credential', () => {
     const mockCredentialOfferUri = 'test-offer-uri';
-    const expectedResponse = { message: 'Credential request successful' } as any;
+    const expectedResponse = {
+      message: 'Credential request successful',
+    } as any;
 
-    service.requestCredential(mockCredentialOfferUri).subscribe(response => {
+    service.requestCredential(mockCredentialOfferUri).subscribe((response) => {
       expect(response).toEqual(expectedResponse);
     });
 
-    const req = httpTestingController.expectOne(`${environment.server_url + environment.server_uri.request_credential_uri}`);
+    const req = httpTestingController.expectOne(
+      `${
+        environment.server_url + environment.server_uri.request_credential_uri
+      }`
+    );
     expect(req.request.method).toBe('POST');
-    expect(req.request.body).toEqual({ credential_offer_uri: mockCredentialOfferUri });
+    expect(req.request.body).toEqual({
+      credential_offer_uri: mockCredentialOfferUri,
+    });
     req.flush(expectedResponse);
   });
 
@@ -117,148 +136,181 @@ describe('WalletService', () => {
     const mockVCReply: VCReply = {
       selectedVcList: [],
       state: 'test-state',
-      redirectUri: 'test-redirect-uri'
+      redirectUri: 'test-redirect-uri',
     };
     const mockResponse = 'VC executed successfully';
 
-    service.executeVC(mockVCReply).subscribe(response => {
+    service.executeVC(mockVCReply).subscribe((response) => {
       expect(response).toEqual(mockResponse);
     });
 
-    const req = httpTestingController.expectOne(`${environment.server_url + environment.server_uri.verifiable_presentation_uri}`);
+    const req = httpTestingController.expectOne(
+      `${
+        environment.server_url +
+        environment.server_uri.verifiable_presentation_uri
+      }`
+    );
     expect(req.request.method).toBe('POST');
     expect(req.request.body).toEqual(mockVCReply);
     req.flush(mockResponse);
   });
 
   it('should fetch all Verifiable Credentials', () => {
-    const mockResponse: VerifiableCredential[] = [{
-      "@context": ["https://www.w3.org/ns/credentials/v1"],
-      "id": "example-credential-id",
-      "type": ["VerifiableCredential", "LEARCredentialEmployee"],
-      "issuer": {
-        "id": "did:web:provider.dome.fiware.dev"
-      },
-      "issuanceDate": "2024-04-02T09:23:22.637345122Z",
-      "validFrom": "2024-04-02T09:23:22.637345122Z",
-      "expirationDate": "2030-01-01T00:00:00Z",
-      "credentialSubject": {
-        "mandate": {
-          "id": "mandateId1",
-          "mandator": {
-            "organizationIdentifier": "orgId1",
-            "commonName": "Example Unit",
-            "emailAddress": "example@test.com",
-            "serialNumber": "serialNumber1",
-            "organization": "Organization Name",
-            "country": "Country"
+    const mockResponse: VerifiableCredential[] = [
+      {
+        '@context': ['https://www.w3.org/ns/credentials/v1'],
+        id: 'example-credential-id',
+        type: ['VerifiableCredential', 'LEARCredentialEmployee'],
+        issuer: {
+          id: 'did:web:provider.dome.fiware.dev',
+        },
+        issuanceDate: '2024-04-02T09:23:22.637345122Z',
+        validFrom: '2024-04-02T09:23:22.637345122Z',
+        expirationDate: '2030-01-01T00:00:00Z',
+        credentialSubject: {
+          mandate: {
+            id: 'mandateId1',
+            mandator: {
+              organizationIdentifier: 'orgId1',
+              commonName: 'Example Unit',
+              emailAddress: 'example@test.com',
+              serialNumber: 'serialNumber1',
+              organization: 'Organization Name',
+              country: 'Country',
+            },
+            mandatee: {
+              id: 'personId1',
+              first_name: 'Jane',
+              last_name: 'Doe',
+              gender: 'female',
+              email: 'example@test.com',
+              mobile_phone: '+0987654321',
+            },
+            power: [
+              {
+                id: 'powerId1',
+                tmf_type: 'Domain',
+                tmf_domain: ['SomeDomain'],
+                tmf_function: 'SomeFunction',
+                tmf_action: ['SomeAction'],
+              },
+            ],
+            life_span: {
+              start_date_time: '2024-04-02T09:23:22.637345122Z',
+              end_date_time: '2030-01-01T00:00:00Z',
+            },
           },
-          "mandatee": {
-            "id": "personId1",
-            "first_name": "Jane",
-            "last_name": "Doe",
-            "gender": "female",
-            "email": "example@test.com",
-            "mobile_phone": "+0987654321"
-          },
-          "power": [
-            {
-              "id": "powerId1",
-              "tmf_type": "Domain",
-              "tmf_domain": ["SomeDomain"],
-              "tmf_function": "SomeFunction",
-              "tmf_action": ["SomeAction"]
-            }
-          ],
-          "life_span": {
-            "start_date_time": "2024-04-02T09:23:22.637345122Z",
-            "end_date_time": "2030-01-01T00:00:00Z"
-          }
-        }
+        },
+        status: CredentialStatus.ISSUED,
       },
-      status: CredentialStatus.ISSUED
-    }];
+    ];
 
-    service.getAllVCs().subscribe(credentials => {
+    service.getAllVCs().subscribe((credentials) => {
       expect(credentials.length).toBeGreaterThan(0);
       expect(credentials).toEqual(mockResponse);
     });
 
-    const req = httpTestingController.expectOne(`${environment.server_url + environment.server_uri.credentials_uri}`);
+    const req = httpTestingController.expectOne(
+      `${environment.server_url + environment.server_uri.credentials_uri}`
+    );
     expect(req.request.method).toBe('GET');
     req.flush(mockResponse);
   });
-
 
   it('should fetch a single Verifiable Credential by id', () => {
     const data = 'test-id';
     const mockResponse: VerifiableCredential = {
-      "@context": ["https://www.w3.org/ns/credentials/v1"],
-      "id": "single-test-credential-id",
-      "type": ["VerifiableCredential", "LEARCredentialEmployee"],
-      "issuer": {
-        "id": "did:web:provider.dome.fiware.dev"
+      '@context': ['https://www.w3.org/ns/credentials/v1'],
+      id: 'single-test-credential-id',
+      type: ['VerifiableCredential', 'LEARCredentialEmployee'],
+      issuer: {
+        id: 'did:web:provider.dome.fiware.dev',
       },
-      "issuanceDate": "2024-04-02T09:23:22.637345122Z",
-      "validFrom": "2024-04-02T09:23:22.637345122Z",
-      "expirationDate": "2030-01-01T00:00:00Z",
-      "credentialSubject": {
-        "mandate": {
-          "id": "mandateId1",
-          "mandator": {
-            "organizationIdentifier": "orgId1",
-            "commonName": "Test Unit",
-            "emailAddress": "single@test.com",
-            "serialNumber": "serialNumber1",
-            "organization": "Organization Name",
-            "country": "Country"
+      issuanceDate: '2024-04-02T09:23:22.637345122Z',
+      validFrom: '2024-04-02T09:23:22.637345122Z',
+      expirationDate: '2030-01-01T00:00:00Z',
+      credentialSubject: {
+        mandate: {
+          id: 'mandateId1',
+          mandator: {
+            organizationIdentifier: 'orgId1',
+            commonName: 'Test Unit',
+            emailAddress: 'single@test.com',
+            serialNumber: 'serialNumber1',
+            organization: 'Organization Name',
+            country: 'Country',
           },
-          "mandatee": {
-            "id": "personId1",
-            "first_name": "Single",
-            "last_name": "Credential",
-            "gender": "other",
-            "email": "single@test.com",
-            "mobile_phone": "+1234567890"
+          mandatee: {
+            id: 'personId1',
+            first_name: 'Single',
+            last_name: 'Credential',
+            gender: 'other',
+            email: 'single@test.com',
+            mobile_phone: '+1234567890',
           },
-          "power": [
+          power: [
             {
-              "id": "powerId1",
-              "tmf_type": "Domain",
-              "tmf_domain": ["SomeDomain"],
-              "tmf_function": "SomeFunction",
-              "tmf_action": ["SomeAction"]
-            }
+              id: 'powerId1',
+              tmf_type: 'Domain',
+              tmf_domain: ['SomeDomain'],
+              tmf_function: 'SomeFunction',
+              tmf_action: ['SomeAction'],
+            },
           ],
-          "life_span": {
-            "start_date_time": "2024-04-02T09:23:22.637345122Z",
-            "end_date_time": "2030-01-01T00:00:00Z"
-          }
-        }
+          life_span: {
+            start_date_time: '2024-04-02T09:23:22.637345122Z',
+            end_date_time: '2030-01-01T00:00:00Z',
+          },
+        },
       },
-      status: CredentialStatus.ISSUED
+      status: CredentialStatus.ISSUED,
     };
 
-    service.getOne(data).subscribe(credential => {
+    service.getOne(data).subscribe((credential) => {
       expect(credential).toEqual(mockResponse);
     });
 
-    const req = httpTestingController.expectOne(`${environment.server_url}/api/vc/1/${data}/format?format=vc_json`);
+    const req = httpTestingController.expectOne(
+      `${environment.server_url}/api/vc/1/${data}/format?format=vc_json`
+    );
     expect(req.request.method).toBe('GET');
     req.flush(mockResponse);
   });
-
 
   it('should delete a Verifiable Credential by id', () => {
     const VC = 'test-vc-id';
     const mockResponse = 'VC deleted successfully';
 
-    service.deleteVC(VC).subscribe(response => {
+    service.deleteVC(VC).subscribe((response) => {
       expect(response).toEqual(mockResponse);
     });
 
-    const req = httpTestingController.expectOne(`${environment.server_url + environment.server_uri.credentials_by_id_uri + VC}`);
+    const req = httpTestingController.expectOne(
+      `${
+        environment.server_url +
+        environment.server_uri.credentials_by_id_uri +
+        VC
+      }`
+    );
     expect(req.request.method).toBe('DELETE');
     req.flush(mockResponse);
+  });
+
+  it('should request a signature for a given credential ID', () => {
+    const credentialId = 'test-credential-id';
+    const expectedResponse = 'Signed Credential';
+
+    service.requestSignature(credentialId).subscribe((response) => {
+      expect(response.body).toEqual(expectedResponse);
+    });
+
+    const req = httpTestingController.expectOne(
+      `${
+        environment.server_url +
+        environment.server_uri.credentials_signed_by_id_uri
+      }?credentialId=${credentialId}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedResponse);
   });
 });
