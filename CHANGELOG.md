90f77c1e9647c90355012e7fc7bd9cbcbda69340
alberto rubio <alberto.rubio@in2.es>
2024-09-05 09:59:32 +0200

    sonar fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-
+## [1.2.2] - unreleased
+### Fixed
+- Translations
+- Multiple Vcs send.
+### Add
+- Pop up error message on unsuccesful login.
+### Updated
+- Update Angular and scanner version.
 
 ## [1.1.0] - unreleased
 ### Fixed
---
+++ src/app/interfaces/global.d.ts
@@ -0,0 +1,24 @@
+// src/types/global.d.ts
+
+interface Window {
+    env: {
+      server_url?: string;
+      iam_url?: string;
+      websocket_url?: string;
+      iam_uri?: string;
+      client_id?: string;
+      scope?: string;
+      grant_type?: string;
+      execute_content_uri?: string;
+      request_credential_uri?: string;
+      verifiable_presentation_uri?: string;
+      credentials_uri?: string;
+      credentials_by_id_uri?: string;
+      credentials_signed_by_id_uri?: string;
+      users_uri?: string;
+      ebsi_did_uri?: string;
+      cbor_uri?: string;
+      websocket_uri?: string;
+    };
+  }
+  
