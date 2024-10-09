f514b302c1e6ac0101ae05777976071b5c75e19c
roger <roger.miret@in2.es>
2024-10-09 13:57:13 +0200

    basic config: runs tests with jest, but fails because syntax is not updated

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+    globalSetup: 'jest-preset-angular/global-setup', //?
+    moduleNameMapper: {
+        "@app/(.*)": "<rootDir>/src/app/$1",
+        "@assets/(.*)": "<rootDir>/src/assets/$1",
+        "@core/(.*)": "<rootDir>/src/app/core/$1",
+        "@env": "<rootDir>/src/environments/environment",
+        "@src/(.*)": "<rootDir>/src/src/$1",
+        "@services/(.*)": "<rootDir>/src/app/core/services/$1",
+        "@helpers/(.*)": "<rootDir>/src/app/helpers/$1",
+        "@shared/(.*)": "<rootDir>/src/app/shared/$1",
+        '^src/(.*)$': '<rootDir>/src/$1'
+      },
+    collectCoverage:true,
+    coverageDirectory: "./coverage/app",
+    coverageReporters: ["lcov", "text-summary", "cobertura", "html"],
+    collectCoverageFrom: [
+      "src/app/**/*.ts",
+      "!<rootDir>/node_modules/",
+      "!<rootDir>/test/",
+      "!src/app/**/*.module.ts",
+    ],
+
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '/src/app/components/vc-view',
+        '/src/app/interceptors',
+        '/src/app/interfaces',
+        '/src/app/pages',
+        '/src/app/services',
+      ]
+  };
