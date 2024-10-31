5ce2b8eb401f1c8ecef54c24c6065180f14e6597
RubenModamioGarcia <ruben.modamio@in2.es>
2024-10-31 12:59:12 +0100

    fix error when user has no credentials

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [v1.3.2] - ()
+### Fixed
+- Error popup is shown when the user has no credentials.
+
 ## [v1.3.1] - ()
 ### Fixed
 - Error popup isn't shown when an already used login QR is used.
--- src/app/pages/credentials/credentials.page.ts
+++ src/app/pages/credentials/credentials.page.ts
@@ -14,8 +14,10 @@ import { DataService } from 'src/app/services/data.service';
 import { VerifiableCredential } from 'src/app/interfaces/verifiable-credential';
 import { CameraLogsService } from 'src/app/services/camera-logs.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { filter } from 'rxjs';
+import {filter, of, throwError} from 'rxjs';
 import { HttpErrorInterceptor } from 'src/app/interceptors/error-handler.interceptor';
+import {catchError} from "rxjs/operators";
+import {HttpErrorResponse} from "@angular/common/http";
 
 const TIME_IN_MS = 3000;
 
@@ -143,6 +145,18 @@ export class CredentialsPage implements OnInit {
   public refresh() {
     this.walletService
       .getAllVCs()
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 404) {
+            console.log('Error 404: Resource not found.');
+            // Retornamos un observable vacío para que no ocurra nada más
+            return of([]);
+          } else {
+            // Si es cualquier otro error, lo lanzamos nuevamente para ser manejado por el suscriptor o seguir un flujo de errores estándar.
+            return throwError(() => error);
+          }
+        })
+      )
       .subscribe((credentialListResponse: VerifiableCredential[]) => {
         this.credList = credentialListResponse.slice().reverse();
       });
@@ -187,7 +201,7 @@ export class CredentialsPage implements OnInit {
           error: (httpErrorResponse) => {
             this.websocket.closeConnection();
             this.toggleScan = true;
-            
+
             const httpErr = httpErrorResponse?.error;
             const message = httpErr?.message || httpErrorResponse?.message || 'No error message';
             const title = httpErr?.title || httpErrorResponse?.title || '(No title)';
