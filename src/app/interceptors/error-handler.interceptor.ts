cc60c137fb9281a265505baf6592c30cde6f34be
roger <roger.miret@in2.es>
2024-10-31 13:21:16 +0100

    fix error-handler interceptor

--- src/app/interceptors/error-handler.interceptor.ts
+++ src/app/interceptors/error-handler.interceptor.ts
@@ -20,21 +20,22 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
-      catchError((error: HttpErrorResponse) => {
-        let errMessage = error.error?.message || error.message || 'Unknown Http error';
+      catchError((errorResp: HttpErrorResponse) => {
+        let errMessage = errorResp.error?.message || errorResp.message || 'Unknown Http error';
 
         if (
           errMessage?.startsWith('There is no credential available')
         ) {
           console.error('Handled silently:', errMessage);
         } 
-        else if (request.url.endsWith(environment.server_uri.execute_content_uri) !&& error.error?.message
-        ) {
-          errMessage = 'There was a problem processing the QR. It might be invalid or already have been used';
-        } else {
-          if (error.status === 404) {
+        else {
+          if (request.url.endsWith('api/error') && (errMessage !== 'The received QR content cannot be processed')
+          ) {
+              errMessage = 'There was a problem processing the QR. It might be invalid or already have been used';
+          }
+          if (errorResp.status === 404) {
             console.error('Resource not found:', errMessage);
-          } else if (error.status === 401) {
+          } else if (errorResp.status === 401) {
             console.error('Unauthorized:', errMessage);
           }  else {
             console.error('An HTTP error occurred:', errMessage);
@@ -42,9 +43,9 @@ export class HttpErrorInterceptor implements HttpInterceptor {
           this.toastServiceHandler
             .showErrorAlert(errMessage)
             .subscribe(); //TODO unsubscribe?
-          console.error('Error occurred:', error);
+          console.error('Error occurred:', errorResp);
         }
-        return throwError(() => error);
+        return throwError(() => errorResp);
       })
     );
   }
