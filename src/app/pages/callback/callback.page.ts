844abada7e447efc60052fcb161c4d4cdcfabf7d
PolCaparrosEscudero <pol.caparros@in2.es>
2024-12-24 14:38:11 +0100

    blurred callball page

--- src/app/pages/callback/callback.page.ts
+++ src/app/pages/callback/callback.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { AuthValidatorService } from '../../services/auth-validator.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-callback',
   templateUrl: './callback.page.html',
   styleUrls: ['./callback.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, TranslateModule]
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
 export class CallbackPage{
