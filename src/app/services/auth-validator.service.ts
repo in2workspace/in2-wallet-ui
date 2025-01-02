f5fec54a8b4157855f5dbc2e5cf0a57a0648fff4
PolCaparrosEscudero <pol.caparros@in2.es>
2025-01-02 09:11:41 +0100

    loader

--- src/app/services/auth-validator.service.ts
+++ src/app/services/auth-validator.service.ts
@@ -5,16 +5,20 @@ import { ActivatedRoute, Router } from '@angular/router';
   providedIn: 'root',
 })
 export class AuthValidatorService {
-  private router = inject(Router);
-  private route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
   private hasRedirected = false;
 
   async validateAuthParams(): Promise<void> {
     if (this.hasRedirected) return;
     
     const urlState = this.route.snapshot.queryParamMap.get('state');
+    const urlCode = this.route.snapshot.queryParamMap.get('code');
+    if (!urlCode && !urlState) {
+      await this.delayedRedirect();
+      return;
+    }
     if (!urlState) {
-      console.error('State no presente en la URL.');
       await this.delayedRedirect();
       return;
     }
@@ -29,11 +33,9 @@ export class AuthValidatorService {
     const storedState = parsedData.authStateControl;
 
     if (urlState !== storedState) {
-      console.error('State no válido. Limpiando y redirigiendo...');
       delete parsedData.authStateControl;
       localStorage.setItem('0-auth-client', JSON.stringify(parsedData));
       await this.delayedRedirect();
-      return;
     }
   }
 
