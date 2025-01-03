1016f7020201baf3b721712b5f916c0cfff3d66c
PolCaparrosEscudero <pol.caparros@in2.es>
2025-01-03 09:35:03 +0100

    try index placeholder style 2

--- src/app/pages/callback/callback.page.ts
+++ src/app/pages/callback/callback.page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { AuthValidatorService } from '../../services/auth-validator.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { Router } from '@angular/router';
 
@@ -15,11 +14,6 @@ import { Router } from '@angular/router';
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
 export class CallbackPage{
-/*   public constructor(private readonly authValidatorService: AuthValidatorService) {}
-
-  ngAfterViewInit(): void {
-    this.authValidatorService.validateAuthParams();
-  } */
     constructor(private readonly router: Router) {}
 
     ngAfterViewInit(): void {
