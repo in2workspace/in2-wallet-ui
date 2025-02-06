44d2c0753cc3c0f20afb4a6c1244095ac9b80aad
roger <roger.miret@in2.es>
2025-02-06 10:15:14 +0100

    fix response status handling

--- src/app/interceptors/error-handler.interceptor.ts
+++ src/app/interceptors/error-handler.interceptor.ts
@@ -23,7 +23,7 @@ export class HttpErrorInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((errorResp: HttpErrorResponse) => {
         let errMessage = errorResp.error?.message || errorResp.message || 'Unknown Http error';
-        let errStatus = errorResp.status || errorResp.error?.status;
+        let errStatus = errorResp.status ?? errorResp.error?.status;
 
         //DONT'T SHOW POPUP CASES
         if ( //todo review this handler
