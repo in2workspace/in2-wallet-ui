f95ce488d0dd3d2718e9764c8bdc67de96057ef9
roger <roger.miret@in2.es>
2025-03-11 16:47:20 +0100

    add comments

--- src/environments/environment.deployment.ts
+++ src/environments/environment.deployment.ts
@@ -1,3 +1,5 @@
+// this file is the used when deploying (see Dockerfile);
+// its values will be overwriten by env variables (see env.js & env.template.js)
 export const environment = {
   production: true,
   server_url: window["env"]["server_url"], // s'ha de validar a Helm
@@ -5,9 +7,9 @@ export const environment = {
   websocket_url: window["env"]["websocket_url"], // s'ha de validar a Helm
   iam_params: {
     iam_uri: window["env"]["iam_uri"], // s'ha de validar a Helm
-    // client_id: window["env"]["client_id"] || 'auth-client',
-    // scope: window["env"]["scope"] || 'openid profile email offline_access',
-    // grant_type: window["env"]["grant_type"] || 'code'
+    // client_id: window["env"]["client_id"] || 'auth-client', //constant
+    // scope: window["env"]["scope"] || 'openid profile email offline_access', //constant
+    // grant_type: window["env"]["grant_type"] || 'code' //constant
   },
   // server_uri: {
     // execute_content_uri: window["env"]["execute_content_uri"] || '/api/v1/execute-content',
@@ -18,7 +20,7 @@ export const environment = {
     // credentials_signed_by_id_uri: window["env"]["credentials_signed_by_id_uri"] || '/api/v1/request-signed-credential',
     // users_uri: window["env"]["users_uri"] || '/api/v1/users', //!treure
     // ebsi_did_uri: window["env"]["ebsi_did_uri"] || '/api/v1/ebsi-did', //!treure
-    // cbor: window["env"]["cbor_uri"] || '/api/v1/vp/cbor'
+    // cbor: window["env"]["cbor_uri"] || '/api/v1/vp/cbor' //constant
   // },
   // websocket_uri: window["env"]["websocket_uri"] || '/api/v1/pin',
   logs_enabled: window["env"]["logs_enabled"] || false, //opcional amb fallback
