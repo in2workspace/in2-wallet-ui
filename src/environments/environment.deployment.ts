577d6d5705e5e0b58be5fd1df6c8cdd2ccc9c079
roger <roger.miret@in2.es>
2025-03-12 09:10:33 +0100

    added string fallback to avoid error from Auth library when building app

--- src/environments/environment.deployment.ts
+++ src/environments/environment.deployment.ts
@@ -2,11 +2,11 @@
 // its values will be overwriten by env variables (see env.js & env.template.js)
 export const environment = {
   production: true,
-  server_url: window["env"]["server_url"], // s'ha de validar a Helm
-  iam_url: window["env"]["iam_url"], // s'ha de validar a Helm
-  websocket_url: window["env"]["websocket_url"], // s'ha de validar a Helm
+  server_url: window["env"]["server_url"] || "", // ! s'ha de validar a Helm --added string fallback to avoid error from Auth library when building
+  iam_url: window["env"]["iam_url"] || "", // ! s'ha de validar a Helm  // ! s'ha de validar a Helm --added string fallback to avoid error from Auth library when building
+  websocket_url: window["env"]["websocket_url"], // ! s'ha de validar a Helm
   iam_params: {
-    iam_uri: window["env"]["iam_uri"], // s'ha de validar a Helm
+    iam_uri: window["env"]["iam_uri"] || "", // ! s'ha de validar a Helm --added string fallback to avoid error from Auth librarywhen building
     // client_id: window["env"]["client_id"] || 'auth-client', //constant
     // scope: window["env"]["scope"] || 'openid profile email offline_access', //constant
     // grant_type: window["env"]["grant_type"] || 'code' //constant
@@ -18,8 +18,8 @@ export const environment = {
     // credentials_uri: window["env"]["credentials_uri"] || '/api/v1/credentials',
     // credentials_by_id_uri: window["env"]["credentials_by_id_uri"] || '/api/v1/credentials?credentialId=',
     // credentials_signed_by_id_uri: window["env"]["credentials_signed_by_id_uri"] || '/api/v1/request-signed-credential',
-    // users_uri: window["env"]["users_uri"] || '/api/v1/users', //!treure
-    // ebsi_did_uri: window["env"]["ebsi_did_uri"] || '/api/v1/ebsi-did', //!treure
+    // users_uri: window["env"]["users_uri"] || '/api/v1/users', // treure
+    // ebsi_did_uri: window["env"]["ebsi_did_uri"] || '/api/v1/ebsi-did', // treure
     // cbor: window["env"]["cbor_uri"] || '/api/v1/vp/cbor' //constant
   // },
   // websocket_uri: window["env"]["websocket_uri"] || '/api/v1/pin',
@@ -32,7 +32,7 @@ export const environment = {
       secondary: window["env"]["secondary"] || '#50c8ff',
       secondary_contrast: window["env"]["secondary_contrast"] || '#000000'
     },
-    logo_src: window["env"]["logo_src"], // s'ha de validar a Helm
+    logo_src: window["env"]["logo_src"], // ! s'ha de validar a Helm
     favicon_src: window["env"]["favicon_src"] || "assets/icon/favicon.png" //opcional amb fallback
   }
 };
