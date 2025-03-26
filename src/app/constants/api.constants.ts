2c871d744589fd92f6ff20df9772be14881677f0
roger <roger.miret@in2.es>
2025-03-26 11:29:52 +0100

    Revert "remove "api/" from server path"
    This reverts commit 6b4c0bb0506cc19740f774db80a62bcd6cc085cc.

--- src/app/constants/api.constants.ts
+++ src/app/constants/api.constants.ts
@@ -1,13 +1,13 @@
-const V1Path = '/v1';
+const apiV1Path = '/api/v1';
 
-export const WEBSOCKET_PATH = `${V1Path}/pin`;
+export const WEBSOCKET_PATH = `${apiV1Path}/pin`;
 
 export const SERVER_PATH = Object.freeze({
-  CBOR : `${V1Path}/vp/cbor`,
-  CREDENTIALS: `${V1Path}/credentials`,
-  CREDENTIALS_BY_ID: `${V1Path}/credentials?credentialId=`,
-  CREDENTIALS_SIGNED_BY_ID: `${V1Path}/request-signed-credential`,
-  EXECUTE_CONTENT: `${V1Path}/execute-content`,
-  REQUEST_CREDENTIAL: `${V1Path}/openid-credential-offer`,
-  VERIFIABLE_PRESENTATION: `${V1Path}/vp`,
+  CBOR : `${apiV1Path}/vp/cbor`,
+  CREDENTIALS: `${apiV1Path}/credentials`,
+  CREDENTIALS_BY_ID: `${apiV1Path}/credentials?credentialId=`,
+  CREDENTIALS_SIGNED_BY_ID: `${apiV1Path}/request-signed-credential`,
+  EXECUTE_CONTENT: `${apiV1Path}/execute-content`,
+  REQUEST_CREDENTIAL: `${apiV1Path}/openid-credential-offer`,
+  VERIFIABLE_PRESENTATION: `${apiV1Path}/vp`,
 });
