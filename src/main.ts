d594f647e4a5f68c7a00bd07f9e6bcdfc1a0c7e5
Roger Miret <roger.miret@in2.es>
2025-07-29 10:59:54 +0200

    main: split "disable touch scroll" into a function

--- src/main.ts
+++ src/main.ts
@@ -21,22 +21,9 @@ import {
 import { HttpErrorInterceptor } from './app/interceptors/error-handler.interceptor';
 import { IAM_PARAMS, IAM_POST_LOGIN_ROUTE, IAM_POST_LOGOUT_URI, IAM_REDIRECT_URI } from './app/constants/iam.constants';
 
-// todo separate into a function; clean up listener
-document.addEventListener(
-  'touchmove',
-  function (event) {
-    const noScrollPages = [
-      '/tabs/settings',
-      '/tabs/home'
-    ];
 
-    const currentPath = new URL(window.location.href).pathname;
-
-    if (noScrollPages.includes(currentPath)) {
-      event.preventDefault();
-    }
-  },
-  { passive: false }
+disableTouchScrollOnPaths(
+  ['/tabs/settings', '/tabs/home']
 );
 
 if (environment.production) {
@@ -88,3 +75,19 @@ bootstrapApplication(AppComponent, {
 export function httpTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
+
+export function disableTouchScrollOnPaths(noScrollPages: string[]) {
+  const handler = (event: TouchEvent) => {
+    const currentPath = new URL(window.location.href).pathname;
+    if (noScrollPages.includes(currentPath)) {
+      event.preventDefault();
+    }
+  };
+
+  document.addEventListener('touchmove', handler, { passive: false });
+
+  return () => {
+    document.removeEventListener('touchmove', handler);
+  };
+}
+
